c8aca74641e6eee43d901e51698ba28fe6bd5875
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2019-06-29 23:43:38 -0400

    Password hashing and compare method. This commit creates password hashing on the user model. This will make it so the user's password is stored as a hash rather than plain text. This commit also creates a compare password method on the user model. This will be available on every user to verify their pasword.

--- models/user.js
+++ models/user.js
@@ -27,6 +27,59 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+/** 
+ * This is a pre save hook for the user model.
+ * It uses bcrypt to hash the user's password.
+ */
+userSchema.pre("save", async function (next) {
+
+    try {
+
+        //If the password has not been modified.
+        if (!this.isModified("password")) {
+
+            //return next.
+            return next();
+        }
+
+        //Use bcrypt to hash the user's password.
+        let hashedPassword = await bcrpyt.hash(this.password, 10);
+
+        //Replace the user's password with the hashed password.
+        this.password = hashedPassword;
+
+        //Return next.
+        return next();
+
+    } catch (err) {
+
+        //Return next with the error.
+        return next(err);
+    }
+});
+
+/** 
+ * Instance method for the user model.
+ * This method determines if the the password is correct.
+ */
+userSchema.method.comparePassword = async function (candidatePassword, next) {
+    try {
+
+        //Use bcrypt to see if the the password is correct.
+        let isMatch = await bcrpyt.compare(candidatePassword, this.password);
+
+        //Return wether of not the password is a match.
+        return isMatch
+
+    } catch (err) {
+
+
+        //Return next with the error.
+        return next(err);
+    }
+
+}
+
 //Create the model using the schema.
 const User = mongoose.model('User', userSchema);
 
