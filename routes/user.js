1abe31aeead63948a6001faed75180de82ba6a21
Jacob Richardson <44219889+JacobRichardson@users.noreply.github.com>
2020-05-07 16:54:10 -0400

    Creates routes/user.js.
    This commit creates the user route. This commit currently has
    the /api/users/:id get route. This route gets a specific user.

---
+++ routes/user.js
@@ -0,0 +1,24 @@
+/** 
+ * User routes All routes prefixed with /api/users/.
+ */
+
+// Imports.
+const express = require('express');
+const {
+    getUser
+} = require('../handlers/user');
+
+// Route with merge params.
+const router = express.Router({
+    mergeParams: true
+});
+
+// General followers route (prefix '/api/users/').
+router.route('/:id')
+
+    // Get route for a specific user.
+    .get(getUser)
+
+
+//Export the router.
+module.exports = router;
